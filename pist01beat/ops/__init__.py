81e20b0ec918884c79930d3e1d2f4525682f336f
nickohlessa-pixel <nickohlessa@gmail.com>
2025-12-29 00:07:20 +0000

    CT-NEXT: normalize ops __all__ and export edge slots schema

--- pist01beat/ops/__init__.py
+++ pist01beat/ops/__init__.py
@@ -3,9 +3,18 @@ from . import util_hash
 
 __all__ = ["util_hash"]
 
-from . import util_hash
-
-__all__ = ["util_hash"]
-
 from . import ops_inventory_cli
 __all__.append("ops_inventory_cli")
+
+# --- edge slots schema (read-only) ---
+from . import edge_slots_schema as _edge_slots_schema
+
+EDGE_SLOTS_SCHEMA_VERSION = _edge_slots_schema.EDGE_SLOTS_SCHEMA_VERSION
+new_edge_slots_payload = _edge_slots_schema.new_edge_slots_payload
+validate_edge_slots = _edge_slots_schema.validate_edge_slots
+
+__all__.extend([
+    "EDGE_SLOTS_SCHEMA_VERSION",
+    "new_edge_slots_payload",
+    "validate_edge_slots",
+])
